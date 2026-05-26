033185f6e3fa3ad8f19fbd35c01de5614993f299
md-abu-kayser <abu.kayser.official@gmail.com>
2026-05-26 23:48:14 +0600

    server clear

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { createServer, Server } from "http";
-
-const server: Server = createServer((req, res) => {
-  console.log(req);
-});
-
-server.listen(5000, () => {
-  console.log("Server is runing on port 5000");
-});
