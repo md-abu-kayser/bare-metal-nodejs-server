70a08b6fd9fbf48a6845ece10277f2198b5deb8f
md-abu-kayser <abu.kayser.official@gmail.com>
2026-06-01 22:16:21 +0600

    feat(routes): add product route handler with basic GET endpoint

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -9,6 +9,8 @@ export const productRoute = (req: IncomingMessage, res: ServerResponse) => {
   if (method === "GET" && url === "/") {
     res.writeHead(200, { "constent-type": "application/json" });
     res.end(JSON.stringify({ message: "This is root url" }));
+  }else if(){
+    
   } else {
     res.writeHead(200, { "constent-type": "application/json" });
     res.end(JSON.stringify({ message: "There is no root route" }));
